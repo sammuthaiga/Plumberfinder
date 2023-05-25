463c3a4d5a08ded5eece70f60513a07848d6f67a
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-05-26 02:25:17 +0300

    feat: add routes for user authentcation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  Rails.application.routes.draw do
+    resources :users, param: :_username
+    post '/auth/login', to: 'authentication#login'
+    get '/*a', to: 'application#not_found'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
